e0cf98645510c94c279dc918c8ed1d42d0049c6a
jjustin08 <justin_noah17@yahoo.ca>
2024-03-10 16:16:48 -0400

    ADD pawn storage

---
+++ MobileTFT/Assets/Scripts/PawnStorage/PawnStorage.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEngine;
+
+public class PawnStorage : MonoBehaviour
+{
+    private List<Slot> slots = new List<Slot>();
+
+    public Pawn tempPawn;
+    void Start()
+    {
+        slots.AddRange(GetComponentsInChildren<Slot>());
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            FillSlot(tempPawn);
+        }
+    }
+
+    public bool FillSlot(Pawn pawn)
+    {
+        foreach (Slot slot in slots) 
+        { 
+            if(!slot.HasPawn())
+            {
+                Pawn newPawn = Instantiate(pawn);
+                slot.PlacePawn(newPawn);
+                return true;
+            }
+        }
+
+        return false;
+    }
+   
+}
