cdff784d7a7a55975df2b24ba6f8ae7910a55715
Justin Bennett <justin_noah17@yahoo.ca>
2024-04-08 11:43:37 -0400

    MOD leader type functional

--- MobileTFT/Assets/Scripts/PawnTypes/LeaderType.cs
+++ MobileTFT/Assets/Scripts/PawnTypes/LeaderType.cs
@@ -6,4 +6,35 @@ using UnityEngine;
 public class LeaderType : Type
 {
     // give stats to everyone or something
+    public float bonusStatPerc1 = 1.2f;
+    public float bonusStatPerc2 = 1.4f;
+    public float bonusStatPerc4 = 1.6f;
+
+    public override void AffectStats(Pawn pawn, int amount)
+    {
+        if (amount >= 4)
+        {
+            foreach(Pawn p in GridUtil.Instance.GetAllPawns(true))
+            {
+                p.GetStats().SetCurrentHealth(p.GetStats().GetCurrentHealth() * bonusStatPerc4);
+            }
+            
+        }
+        else if (amount >= 2)
+        {
+            foreach (Pawn p in GridUtil.Instance.GetAllPawns(true))
+            {
+                p.GetStats().SetCurrentHealth(p.GetStats().GetCurrentHealth() * bonusStatPerc2);
+            }
+
+        }
+        else if(amount >= 1)
+        {
+            foreach (Pawn p in GridUtil.Instance.GetAllPawns(true))
+            {
+                p.GetStats().SetCurrentHealth(p.GetStats().GetCurrentHealth() * bonusStatPerc1);
+            }
+
+        }
+    }
 }
