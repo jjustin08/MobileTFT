276c581d219e29f01434c04527f581a90e8e720c
Sangmin-Jeong <serim745@gmail.com>
2024-05-19 17:46:38 -0400

    [MOD] MainMenu Buttons

--- MobileTFT/Assets/Scripts/UI/Menus/MainMenu.cs
+++ MobileTFT/Assets/Scripts/UI/Menus/MainMenu.cs
@@ -7,88 +7,89 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private Button singleplayerButton;
-    [SerializeField] private Button multiplayerButton;
-    [SerializeField] private Button tutorialButton;
-    [SerializeField] private GameObject items;
-
-    [SerializeField] private GameObject tutorialScreen;
-    [SerializeField] private Button tutorialBackButton;
-
-    [SerializeField] private GameObject basicsScreen;
-    [SerializeField] private Button basicsButton;
-    [SerializeField] private GameObject typesScreen;
-    [SerializeField] private Button typesButton;
-    [SerializeField] private GameObject pawnsScreen;
-    [SerializeField] private Button pawnsButton;
-
-    [SerializeField] private Button pawnsBackButton;
-    [SerializeField] private Button typesBackButton;
-    [SerializeField] private Button basicsBackButton;
-
-    [SerializeField] private LSS_Manager LssManager;
-
-    private void Start()
-    {
-        singleplayerButton.onClick.AddListener(singleplayerButtonCLick);
-        multiplayerButton.onClick.AddListener(multplayerplayerButtonCLick);
-        tutorialButton.onClick.AddListener(TutorialButtonCLick);
-        tutorialBackButton.onClick.AddListener(BackButtonToMainMenuClick);
-
-        basicsButton.onClick.AddListener(BasicsButtonClick);
-        typesButton.onClick.AddListener(TypesButtonClick);
-        pawnsButton.onClick.AddListener(PawnsButtonClick);
-
-        pawnsBackButton.onClick.AddListener(BackButtonToTutorialClick);
-        typesBackButton.onClick.AddListener(BackButtonToTutorialClick);
-        basicsBackButton.onClick.AddListener(BackButtonToTutorialClick);
-    }
-
-    private void singleplayerButtonCLick()
-    {
-        //SceneManager.LoadScene("SinglePlayer");
-        LssManager.LoadScene("SinglePlayer");
-    }
-    
-    private void multplayerplayerButtonCLick()
-    {
-        //SceneManager.LoadScene("Multiplayer");
-        LssManager.LoadScene("Multiplayer");
-    }
-    
-    private void TutorialButtonCLick()
-    {
-        items.SetActive(false);
-        tutorialScreen.SetActive(true);
-    }
-    
-    private void BackButtonToMainMenuClick()
-    {
-        items.SetActive(true);
-        tutorialScreen.SetActive(false);
-    }
-    
-    private void BackButtonToTutorialClick()
-    {
-        tutorialScreen.SetActive(true);
-        basicsScreen.SetActive(false);
-        typesScreen.SetActive(false);
-        pawnsScreen.SetActive(false);
-    }
-
-    private void BasicsButtonClick()
-    {
-        tutorialScreen.SetActive(false);
-        basicsScreen.SetActive(true);
-    }
-    private void TypesButtonClick()
-    {
-        tutorialScreen.SetActive(false);
-        typesScreen.SetActive(true);
-    }
-    private void PawnsButtonClick()
-    {
-        tutorialScreen.SetActive(false);
-        pawnsScreen.SetActive(true);
-    }
+    // [SerializeField] private Button singleplayerButton;
+    // [SerializeField] private Button multiplayerButton;
+    // [SerializeField] private Button tutorialButton;
+    // [SerializeField] private GameObject items;
+    //
+    // [SerializeField] private GameObject tutorialScreen;
+    // [SerializeField] private Button tutorialBackButton;
+    //
+    // [SerializeField] private GameObject basicsScreen;
+    // [SerializeField] private Button basicsButton;
+    // [SerializeField] private GameObject typesScreen;
+    // [SerializeField] private Button typesButton;
+    // [SerializeField] private GameObject pawnsScreen;
+    // [SerializeField] private Button pawnsButton;
+    //
+    // [SerializeField] private Button pawnsBackButton;
+    // [SerializeField] private Button typesBackButton;
+    // [SerializeField] private Button basicsBackButton;
+    //
+    // [SerializeField] private LSS_Manager LssManager;
+    //
+    // private void Start()
+    // {
+    //     // singleplayerButton.onClick.AddListener(singleplayerButtonCLick);
+    //     // multiplayerButton.onClick.AddListener(multplayerplayerButtonCLick);
+    //     // //tutorialButton.onClick.AddListener(TutorialButtonCLick);
+    //     // tutorialBackButton.onClick.AddListener(BackButtonToMainMenuClick);
+    //     //
+    //     // basicsButton.onClick.AddListener(BasicsButtonClick);
+    //     // typesButton.onClick.AddListener(TypesButtonClick);
+    //     // pawnsButton.onClick.AddListener(PawnsButtonClick);
+    //     //
+    //     // pawnsBackButton.onClick.AddListener(BackButtonToTutorialClick);
+    //     // typesBackButton.onClick.AddListener(BackButtonToTutorialClick);
+    //     // basicsBackButton.onClick.AddListener(BackButtonToTutorialClick);
+    // }
+    //
+    // private void singleplayerButtonCLick()
+    // {
+    //     //SceneManager.LoadScene("SinglePlayer");
+    //     LssManager.LoadScene("SinglePlayer");
+    // }
+    //
+    // private void multplayerplayerButtonCLick()
+    // {
+    //     //SceneManager.LoadScene("Multiplayer");
+    //     LssManager.LoadScene("Multiplayer");
+    // }
+    //
+    //
+    // public void TutorialButtonCLick()
+    // {
+    //     items.SetActive(false);
+    //     tutorialScreen.SetActive(true);
+    // }
+    //
+    // public void BackButtonToMainMenuClick()
+    // {
+    //     items.SetActive(true);
+    //     tutorialScreen.SetActive(false);
+    // }
+    //
+    // public void BackButtonToTutorialClick()
+    // {
+    //     tutorialScreen.SetActive(true);
+    //     basicsScreen.SetActive(false);
+    //     typesScreen.SetActive(false);
+    //     pawnsScreen.SetActive(false);
+    // }
+    //
+    // private void BasicsButtonClick()
+    // {
+    //     tutorialScreen.SetActive(false);
+    //     basicsScreen.SetActive(true);
+    // }
+    // private void TypesButtonClick()
+    // {
+    //     tutorialScreen.SetActive(false);
+    //     typesScreen.SetActive(true);
+    // }
+    // private void PawnsButtonClick()
+    // {
+    //     tutorialScreen.SetActive(false);
+    //     pawnsScreen.SetActive(true);
+    // }
 }
